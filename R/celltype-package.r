1cc3ae7d7686674667a08b1fcb791ac1f5fc44a5
Diego Diez <diego10ruiz@gmail.com>
2018-09-09 16:51:29 +0900

    Fix dependencies.

--- R/celltype-package.r
+++ R/celltype-package.r
@@ -6,4 +6,6 @@
 #' @import tidyr dplyr tibble
 #' @importFrom d10misc to_tidy
 #' @importFrom stats cor
+#' @importFrom Biobase exprs featureData pData
+#' @importFrom SummarizedExperiment assay rowData colData
 NULL
