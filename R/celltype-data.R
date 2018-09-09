2b81b13a1d02d564a4afc1a8bf3c1a6929a992d6
Diego Diez <diego10ruiz@gmail.com>
2018-09-09 17:03:50 +0900

    Add datasets documentation.

---
+++ R/celltype-data.R
@@ -0,0 +1,28 @@
+#' Immune cell markers
+#'
+#' @name markers
+#' @docType data
+#' @keywords data
+NULL
+
+
+#' Immune cell dictionary from Immgen.
+#'
+#' @name immgen.db
+#' @docType data
+#' @keywords data
+NULL
+
+#' Immune cell dictionary from Immuno-Navigator.
+#'
+#' @name immnav.db
+#' @docType data
+#' @keywords data
+NULL
+
+#' SingleCellExperiment example dataset.
+#'
+#' @name sce1
+#' @docType data
+#' @keywords data
+NULL
