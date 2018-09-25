f7a677267e34653289b77088918594835d18c1e3
Diego Diez <diego10ruiz@gmail.com>
2018-09-25 12:06:24 +0900

    Fix errors in tests.

--- tests/testthat/test-predict_celltype.R
+++ tests/testthat/test-predict_celltype.R
@@ -5,6 +5,6 @@ celltype <- predict_celltype(celltype::sce1)
 test_that("predict_celltype works", {
   expect_is(celltype, "matrix")
   expect_type(celltype, "double")
-  expect_equal(dim(celltype), c(217, 500))
+  expect_equal(dim(celltype), c(199, 500))
   expect_equal(celltype[1, 1], 0.6096284, tolerance = 0.001)
 })
--- tests/testthat/test-to_matrix.R
+++ tests/testthat/test-to_matrix.R
@@ -1,12 +1,12 @@
 context("to_matrix")
 
-db <- get_db()
+db <- get_db(org = "human")
 db <- to_matrix(db)
 
 test_that("to_matrix works", {
   expect_is(db, "matrix")
   expect_is(db[1, 1], "numeric")
-  expect_equal(dim(db), c(61, 217))
+  expect_equal(dim(db), c(61, 199))
   expect_equal(rownames(db[1:3, , drop = FALSE]), c("BCR", "CCR7", "CD14"))
   expect_equal(colnames(db[, 1:3, drop = FALSE]), c("B.FO.LN", "B.FO.MLN", "B.FO.PC"))
 })
