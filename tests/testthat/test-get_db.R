54f8787f6ce971ca4875417cfa194831d7986954
Diego Diez <diego10ruiz@gmail.com>
2018-10-27 20:31:54 +0900

    Add test to get_db() for tissue filtering argument.

--- tests/testthat/test-get_db.R
+++ tests/testthat/test-get_db.R
@@ -8,3 +8,13 @@ test_that("get_db works", {
   expect_is(db[["symbol"]], "character")
   expect_is(db[["expression"]], "numeric")
 })
+
+db <- get_db(tissue = "LN")
+
+test_that("get_db works with tissue", {
+  expect_is(db, "data.frame")
+  expect_is(db[["celltype"]], "character")
+  expect_is(db[["symbol"]], "character")
+  expect_is(db[["expression"]], "numeric")
+  expect_equal(nrow(db), 12139)
+})
